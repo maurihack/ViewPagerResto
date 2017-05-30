e3ea0ca2d34673a46a362a2682cd934e6507ec10
Mauricio Velez <mauriciovelezmd@gmail.com>
2017-05-29 23:10:11 -0300

    Actualizacion datos restos

--- app/src/main/java/com/example/dh2/restaurants/AdapterViewPager.java
+++ app/src/main/java/com/example/dh2/restaurants/AdapterViewPager.java
@@ -19,13 +19,14 @@ public class AdapterViewPager extends FragmentStatePagerAdapter {
     public AdapterViewPager(FragmentManager fm) {
         super(fm);
 
-        fragments.add(RestaurantsFragment.fabricameElFragment(R.drawable.tegui, "Tegui"));
-        fragments.add(RestaurantsFragment.fabricameElFragment(R.drawable.astridygaston, "Astrid Gaston"));
-        fragments.add(RestaurantsFragment.fabricameElFragment(R.drawable.borago, "Borago"));
-        fragments.add(RestaurantsFragment.fabricameElFragment(R.drawable.dom, "Dom"));
-        fragments.add(RestaurantsFragment.fabricameElFragment(R.drawable.maido, "Maido"));
-        fragments.add(RestaurantsFragment.fabricameElFragment(R.drawable.mani, "Mani"));
-        fragments.add(RestaurantsFragment.fabricameElFragment(R.drawable.quintonil, "Quintonil"));
+        fragments.add(RestaurantsFragment.fabricameElFragment(R.drawable.tegui, "Tegui \n (Buenos Aires - Argentina"));
+        fragments.add(RestaurantsFragment.fabricameElFragment(R.drawable.astridygaston, "Astrid y  Gastón\n(Lima - Peru)"));
+        fragments.add(RestaurantsFragment.fabricameElFragment(R.drawable.borago, "Boragó\n(Santiago - Chile)"));
+        fragments.add(RestaurantsFragment.fabricameElFragment(R.drawable.dom, "D.O.M.\n(San Pablo - Brasil)"));
+        fragments.add(RestaurantsFragment.fabricameElFragment(R.drawable.maido, "Maido\n(Lima - Peru)"));
+        fragments.add(RestaurantsFragment.fabricameElFragment(R.drawable.mani, "Maní\n(San Pablo - Brasil)"));
+        fragments.add(RestaurantsFragment.fabricameElFragment(R.drawable.quintonil, "Quintonil\n (DF - Mexico)"));
+        fragments.add(RestaurantsFragment.fabricameElFragment(R.drawable.central, "Central\n (Lima - Perú)"));
 
     }
 
